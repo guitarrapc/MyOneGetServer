6ed0a71bfd5c00329ce615b416455c48afa1cd21
guitarrapc <cherishacquire@outlook.com>
2015-09-09 03:01:11 +0900

    Azure Web App Kudu Custom deployment

---
+++ BuildScript/deploy.ps1
@@ -0,0 +1,21 @@
+﻿# sync "MyOneGetServer.Script" folder to wwwroot\MyOneGetServer.Script
+$source = Join-Path (pwd).Path "MyOneGetServer.Script";
+$destination = Join-Path $env:WEBROOT_PATH "MyOneGetServer.Script";
+
+# create content path beforehand
+if (-not (Test-Path $destination))
+{
+    mkdir -Path $destination -Force;
+}
+
+try
+{
+    # msdeploy can not be used. Use robocopy to sync folder
+    . Robocopy.exe $source $destination /MIR /FFT /Z /XA:H /W:5
+    exit 0
+}
+catch
+{
+    Write-Error $_
+    exit 1
+}
